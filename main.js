f8f096afc75d6f02b1b7c5272457f6b65f85048a
Fire-45 <77906966+Fire-45@users.noreply.github.com>
2021-10-12 19:59:41 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+function Info(){
+    window.alert("To use this website, scroll down and click on the Take picture then take a polar bear or cat or panda stuffy and put it in front of the camera, then click on the identify button so you can see what stuffy it is");
+}
